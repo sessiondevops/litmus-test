18d2dfe7fc60ca8f87a0a4ca44efc91e5265a320
sessiondevops <76641683+sessiondevops@users.noreply.github.com>
2022-03-07 16:59:02 +0530

    Update build.groovy

--- kubernetes/workflows/build.groovy
+++ kubernetes/workflows/build.groovy
@@ -2,6 +2,11 @@ pipeline{
 	agent any
 	stages
 	{
+	stage('Checkout Source') {
+      steps {
+        git url:'https://github.com/sessiondevops/litmus-test.git', branch:'main'
+      }
+    }
 	stage('Deploy App') {
       steps {
         script {
