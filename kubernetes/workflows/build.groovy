3c7dcbcc77a2ce8a6d1dcf20ed41a06543174eac
sessiondevops <76641683+sessiondevops@users.noreply.github.com>
2022-03-07 15:13:48 +0530

    Update build.groovy

--- kubernetes/workflows/build.groovy
+++ kubernetes/workflows/build.groovy
@@ -9,10 +9,10 @@ pipeline{
 		stage("K8 Nodes"){
 			steps{
 				kubernetesDeploy(
-					sh "pwd"
 					kubeconfigId: 'k8-config',
 					//enableConfigsubstituion: true
 					enableConfigSubstitution: true
+					kubectl get nodes
 				)
 			}
 		}
